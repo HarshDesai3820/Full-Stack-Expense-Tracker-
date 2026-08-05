3064d17ae0ed78d4533d00539c94bf8fb9181ec9
HarshDesai3820 <desaiharshad004@gmail.com>
2026-08-05 15:30:33 +0530

    Add files via upload

---
+++ backend/routes/expenseRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const protect = require("../middleware/authMiddleware");
+const {
+  getExpenses,
+  createExpense,
+  updateExpense,
+  deleteExpense
+} = require("../controllers/expenseController");
+
+const router = express.Router();
+
+router.use(protect);
+
+router.get("/", getExpenses);
+router.post("/", createExpense);
+router.put("/:id", updateExpense);
+router.delete("/:id", deleteExpense);
+
+module.exports = router;
